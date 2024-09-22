ad5bcf265f952384ff12834c2c0c0428e29d3b87
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-09-22 16:21:54 +0600

    Find Max number in an array using for loop

--- Basic/Find_Max_Num_In_Arr.js
+++ Basic/Find_Max_Num_In_Arr.js
@@ -1,5 +1,12 @@
 const Find_Max_Num_Arr =(arr) =>{
-   return Math.max(...arr);
+//    return Math.max(...arr);
+    let maximum = arr[0];
+    for(let i=1; i<arr.length; i++){
+        if(maximum < arr[i]){
+            maximum = arr[i];
+        }
+    }
+    return maximum;
 }
 
 const Array= [10, 12, 13, 17, 5, 9];
