ecc8ecab15a132a8e8a67c2331c078b43ddbc8f7
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-09-22 18:35:17 +0600

    Update Calculate Factorial

--- Basic/Factorial.js
+++ Basic/Factorial.js
@@ -1,11 +1,14 @@
 function Factorial(num){
     let result=1;
+    if(num === 0){
+        return result;
+    }
     for(let i=1; i<=num; i++){
         result *= i;
     }
     return result;
 }
 
-let input= 4;
+let input= 0;
 const result = Factorial(input);
 console.log(result);
