da24b48eb8cac70f767e2445f3f9e90093cd25f0
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-01-13 05:34:40 +0600

    Swap string problem is solved

---
+++ Basic/SwapString.js
@@ -0,0 +1,10 @@
+function first_last(str1){
+    if(str1 <= 1){
+        return str1;
+    }
+    
+    mid_char = str1.slice(1, str1.length - 1);
+    return(str1.charAt(str1.length - 1)) + mid_char+str1.charAt(0);
+}
+
+console.log(first_last("ab"));
