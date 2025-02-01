dd5e398c3be29f14da0dc4864bd6ce3c9ed44931
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-02-01 17:25:05 +0600

    Find all the keys another way

--- Object/problem01.js
+++ Object/problem01.js
@@ -12,3 +12,13 @@ function isObject(obj){
 }
 
 console.log(_keys({red: "#od", green: "dd"}))
+
+// Another way to get all the keys 
+const object01 = {
+    name: "kabirul",
+    id: "011202026"
+}
+
+for(let key in object01){
+    console.log(key);
+}
